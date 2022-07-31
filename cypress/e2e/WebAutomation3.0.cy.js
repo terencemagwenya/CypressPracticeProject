580a394e6fa7df3065814119aa8a232b21ad5644
terencemagwenya <terrymagwenya@gmail.com>
2022-07-31 07:10:51 +0200

    Elements Selection

--- cypress/e2e/WebAutomation3.0.cy.js
+++ cypress/e2e/WebAutomation3.0.cy.js
@@ -1,5 +1,19 @@
 describe('Web Testing', () => {
-  it('passes', () => {
+  it('Elements selection', () => {
     cy.visit('https://demoqa.com/')
+
+    // Selecting the Elements 
+    cy.get('.category-cards > :nth-child(1)').click()
+
+    // Selecting text box 
+    cy.contains('Text Box').click()
+    cy.get('#userName').type('Terence Magwenya{Enter}')
+    cy.get('#userEmail').type('terrymagwenya@gmail.com')
+    cy.get('#currentAddress').type('266 Rockwood Concession Mazowe')
+    cy.get('#permanentAddress').type('266 Rockwood concession Mazowe Zimbabwe')
+    cy.get('#submit').click()
+    cy.get('#name').should('contain', 'Name:Terence Magwenya')
+
+
   })
 })
